e47ac035669e317dccca918838c75df49df3856f
falconraptor <falconraptor@users.noreply.github.com>
2014-09-16 16:24:38 -0700

    Update Gui.java

--- src/com/falconraptor/timekeeper/init/Gui.java
+++ src/com/falconraptor/timekeeper/init/Gui.java
@@ -1,3 +1,28 @@
 package com.falconraptor.timekeeper.init;
-public class Gui{
+import javax.swing.*;
+public class Gui extends JFrame{
+  public Gui(){
+    super("Timekeeper");
+    setContentPane(setGui());
+    setVisible(true);
+    setLocationRelativeTo(null);
+    pack();
+  }
+  public JPanel setGui(){
+    JPanel p=new JPanel(new GridLayout(1,1,0,0));
+    JButton extras=new JButton("Extras");
+    extras.addActionListener(clicked("extras"));
+    p.add(extras);
+    return p;
+  }
+  public ActionListener clicked(final String button){
+    return new ActionListener(){
+      @Override
+      public run(actionevent ea){
+        if (button.equals("extras")){
+          
+        }
+      }
+    }
+  }
 }
